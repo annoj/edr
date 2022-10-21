6b3c376d8978134fec9a25a5ce749619fde94723
Jona Heitzer <jona.heitzer@gmx.de>
2022-10-21 19:26:04 +0000

    Use perf_buffer to poll process starts from user space

--- src/traceproc.bpf.c
+++ src/traceproc.bpf.c
@@ -6,18 +6,38 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-// int my_pid = 0;
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
+} pb SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, struct event);
+} heap SEC(".maps");
 
 SEC("tp/sched/sched_process_exec")
 int handle_exec(struct trace_event_raw_sched_process_exec /* vmlinux.h */ *ctx)
 {
-    pid_t pid;
-    u64 ts;
+	unsigned fname_off = ctx->__data_loc_filename & 0xffff;
+	struct event *e;
+	int zero = 0;
 
-    pid = bpf_get_current_pid_tgid() >> 32;
-    ts = bpf_ktime_get_ns();
+	// This should allocate/reserve memory for e
+	e = bpf_map_lookup_elem(&heap, &zero);
+	if (!e) {
+		return 0;
+	}
 
-    bpf_printk("Hello from PID: %d (TS: %d).\n", pid, ts);
+	e->pid = bpf_get_current_pid_tgid() >> 32; // Shift to only use pid
+	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	bpf_probe_read_str(&e->filename, sizeof(e->filename),
+					   (void *)ctx + fname_off);
+
+	bpf_perf_event_output(ctx, &pb, BPF_F_CURRENT_CPU, e, sizeof(*e));
 
     return 0;
 }
