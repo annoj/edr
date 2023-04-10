d2229ce75a171f795b3fa52ba130ab64aaa3de7a
Jona Heitzer <jona.heitzer@gmx.de>
2023-04-10 21:48:49 +0000

    Clean up some stuff

--- src/proc.h
+++ src/proc.h
@@ -3,19 +3,20 @@
 
 #define TASK_COMM_LEN 16
 #define MAX_FILENAME_LEN 512
-#define MAX_COMMANDLINE_LEN 2048
+#define MAX_CMDLINE_LEN 2048
 
 struct event {
 	unsigned int pid;
 	unsigned int ppid;
+    unsigned int tgid;
 	unsigned int uid;
 	unsigned int gid;
 	unsigned int loginuid;
 	unsigned int sessionid;
 	char comm[TASK_COMM_LEN];
 	char filename[MAX_FILENAME_LEN];
-	char commandline[MAX_COMMANDLINE_LEN];
-	unsigned int commandline_len;
+	char cmdline[MAX_CMDLINE_LEN];
+	unsigned int cmdline_len;
 };
 
 void *trace_proc(void *exiting);
