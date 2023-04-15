3089da4c677327fce2a7c40b80fd6b78b60eaa6c
Jona Heitzer <jona.heitzer@gmx.de>
2023-04-15 22:12:24 +0000

    Add compile_commands.json

--- generate-compile-commands-json.sh
+++ generate-compile-commands-json.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-pushd src
-
 make --always-make --dry-run \
  | grep -wE 'gcc|g\+\+|clang|cc' \
  | grep -w '\-c' \
  | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]' \
  > compile_commands.json
-
-popd
